dfbfae4af4e283a7dd140e787b81d61035b0a030
MANSI <mnasijais2000@gmail.com>
2024-05-24 04:31:58 +0530

    first commit

---
+++ src/bank/system/Conn.java
@@ -0,0 +1,24 @@
+package bank.system;
+
+
+import java.sql.*;
+
+
+public class Conn {
+    Connection connection;
+    Statement statement;
+
+    public  Conn(){
+        try {
+            connection=DriverManager.getConnection( "jdbc:mysql://localhost:3306/bankSystem","root","12345678");
+            statement = connection.createStatement();
+
+
+        }catch (Exception e){
+            System.out.println("Connection error");
+        }
+
+    }
+
+
+}
